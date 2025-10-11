de391ad547fb3d50f5e8abf685e30792c3a27b66
NurDinBuy <94064993+NurDinBuy@users.noreply.github.com>
2025-10-11 15:07:31 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ const postData = async (url, data) => {
 
 
 
-const url = 'https://9d3622ab603b.ngrok-free.app/register/'
+const url = 'https://cbb20cd644f6.ngrok-free.app/register/'
 
 const bindPostData = (form) => {
   button.onclick = async (event) => {
